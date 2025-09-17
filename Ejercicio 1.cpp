dedc6b167e24893e19818e1f3abe155f0f60a7fc
DuvanTobar <duvan.tobar-s@mail.escuelaing.edu.co>
2025-09-17 04:23:29 +0000

    Ciclo Hecho

---
+++ Ejercicio 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int v1;
+    printf("Bienvenido al programa de suma del vector!");
+    cout << "Por favor ingrese 5 numeros enteros:";
+    for(int i=0;i<5;i++){
+        cin >> v1[i];
+    }
+    v1[5]=[0,0,0,0,0 ];
+}
