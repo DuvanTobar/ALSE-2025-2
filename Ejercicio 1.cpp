b7164a4749f3966828f6322572a677b2fbb7ac95
DuvanTobar <duvan.tobar-s@mail.escuelaing.edu.co>
2025-09-17 04:31:42 +0000

    Programa Final.

--- Ejercicio 1.cpp
+++ Ejercicio 1.cpp
@@ -3,10 +3,13 @@ using namespace std;
 int main()
 {
     int v1;
-    printf("Bienvenido al programa de suma del vector!");
+    printf("Bienvenido al programa de suma de elementos de un vector!");
     cout << "Por favor ingrese 5 numeros enteros:";
     for(int i=0;i<5;i++){
         cin >> v1[i];
     }
-    v1[5]=[0,0,0,0,0 ];
+    v1[5]=[0,0,0,0,0];
+    cout << "La suma de los elementos del vector es:" << v1[0]+v1[1]+v1[2]+v1[3]+v1[4];
+    printf("\n***************FIN DEL PROGRAMA!*******************");
+
 }
