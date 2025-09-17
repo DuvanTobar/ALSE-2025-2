2313d35934cf3c5bdd86da4f84710c87bdd89eff
DuvanTobar <duvan.tobar-s@mail.escuelaing.edu.co>
2025-09-17 04:47:01 +0000

    Declaracion variables.

---
+++ Ejercicio 8,1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    printf("Bienvenido al programa para calcular el interes compuesto!");
+    float mf, ci, tia;
+    int n,t;
+    return 0;
+}
