a6d1119e8f4cd46780c097c55b7decae8e40b02e
DuvanTobar <duvan.tobar-s@mail.escuelaing.edu.co>
2025-09-17 04:33:37 +0000

    Programa Final Corregido.

--- Ejercicio 1.cpp
+++ Ejercicio 1.cpp
@@ -11,5 +11,5 @@ int main()
     v1[5]=[0,0,0,0,0];
     cout << "La suma de los elementos del vector es:" << v1[0]+v1[1]+v1[2]+v1[3]+v1[4];
     printf("\n***************FIN DEL PROGRAMA!*******************");
-
+    return 0;
 }
