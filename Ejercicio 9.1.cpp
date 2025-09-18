fd0ed1784f74d3e5c47d1fb8898cc24f9c624587
DuvanTobar <duvan.tobar-s@mail.escuelaing.edu.co>
2025-09-18 01:41:32 +0000

    Ejercicio M.C.D Finalizado.

--- Ejercicio 9.1.cpp
+++ Ejercicio 9.1.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main()
 {
-    int a,b,c,d;
+    int a,b,c,mcd;
     printf("Bienvenido al programa que calcula el maximo comun divisor de 2 numeros.");
+    printf("\nEn este programa se utiliza el algoritmo de Euclides para calcular el maximo comun divisor.");
     printf("\nSe le Solicitara digitar 2 numeros enteros divisibles entre si y respetar el numerador y denominador(No colocar 0).");
     cout << "\nDigite el primer numero:"; cin >> a;
     cout << "\nAhora digite el segundo numero:"; cin >> b;
@@ -15,8 +16,10 @@ int main()
     }
     if (a%b==0)
     {
-        d=b;
-        cout << "\nEl maximo comun divisor es: " << d;
+        mcd=b;
+        cout << "\nEl maximo comun divisor entre los numeros digitados es:" << mcd;
+        printf("\nGracias por ejecutar el programa!");
+        printf("\n*****************FIN DE LA EJECUCION*****************");
     }
     return 0;
 }
