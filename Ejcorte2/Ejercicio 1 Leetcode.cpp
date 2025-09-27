dd9ce951d038bfd2684f4d064f9351503bd4f673
DuvanTobar <duvan.tobar-s@mail.escuelaing.edu.co>
2025-09-27 03:22:31 +0000

    Inicio Codigo.

---
+++ Ejcorte2/Ejercicio 1 Leetcode.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int vnum[a];
+    printf("Bienvenido al programa que calcula los indices de 2 numeros para sumarlos!");
+    cout << "\nPor favor digite el numero de elementos que desea tener en el arreglo:"; cin >> a;
+    
+
+}
