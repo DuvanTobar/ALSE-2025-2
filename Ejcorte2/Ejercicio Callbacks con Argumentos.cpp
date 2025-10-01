dda69af83298d127a02d893871cf94c2e04bf3e0
DuvanTobar <duvan.tobar-s@mail.escuelaing.edu.co>
2025-10-01 04:30:06 +0000

    Ejercicio Casi terminado.

--- Ejcorte2/Ejercicio Callbacks con Argumentos.cpp
+++ Ejcorte2/Ejercicio Callbacks con Argumentos.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-
     printf("Bienvenido al programa para efectuar operaciones matematicas!\n");
     if (argc != 4)
     {
@@ -15,24 +15,24 @@ int main(int argc, char *argv[])
         cout << "Suma: +\nResta: -\nMultiplicacion: *\nDivision: /\n";
         return 1;
     }
-    try
+     try
     {
         float num1 = stof(argv[1]);
-        float  num2 = stof(argv[3]);
+        float num2 = stof(argv[3]);
         string operacion = argv[2];
         float res;
 
         if (operacion == "+")
         {
-            res=num1 + num2;
+            res = num1 + num2;
         }
         else if (operacion == "-")
         {
-            res=num1 - num2;
+            res = num1 - num2;
         }
         else if (operacion == "*")
         {
-            res=num1 * num2;
+            res = num1 * num2;
         }
         else if (operacion == "/")
         {
@@ -43,4 +43,23 @@ int main(int argc, char *argv[])
             }
             res = num1 / num2;
         }
+        else
+        {
+            cout << "ERROR, la operacion digitada no es valida." << endl;
+            return 1;
+        }
 
+        cout << "El resultado de la operacion es: " << res << endl;
+        return 0;
+    }
+    catch (const invalid_argument &e)
+    {
+        cerr << "ERROR, uno de los valores ingresados no es una estructura de numero valido." << endl;
+        return 1;
+    }
+    catch (const out_of_range &e)
+    {
+        cerr << "ERROR, uno de los números digitados no se encuentra dentro del rango." << endl;
+        return 1;
+    }
+}
